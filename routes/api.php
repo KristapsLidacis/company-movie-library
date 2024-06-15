7fc8af3929e2fc0b6087647e4996d6e9a8b4177e
Kristaps Līdacis <lidacisk@gmail.com>
2024-06-15 11:23:32 +0300

    Add to Movie,MovieBroadcast/MovieMovieBroadcasts routes sanctum middleware

--- routes/api.php
+++ routes/api.php
@@ -7,14 +7,17 @@ use App\Http\Controllers\Api\MovieMovieBoradcastsController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
-Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
-Route::apiResource('movies', MovieController::class);
-
-Route::get('movie-broadcasts', [MovieBoradcastController::class, 'index'])->name('movie-broadcasts.index');
-Route::get('movie-broadcasts/{movie_broadcast}', [MovieBoradcastController::class, 'show'])->name('movie-broadcasts.show');
-Route::post('movie-broadcasts', [MovieBoradcastController::class, 'store'])->name('movie-broadcasts.store');
-
-Route::get('movies/{movie}/movie-broadcasts', [MovieMovieBoradcastsController::class, 'index'])->name('movies.movie-broadcasts.index');
-Route::get('movies/{movie}/movie-broadcasts/{movie_broadcast}', [MovieMovieBoradcastsController::class, 'show'])->name('movies.movie-broadcasts.show');
-Route::post('movies/{movie}/movie-broadcasts', [MovieMovieBoradcastsController::class, 'store'])->name('movies.movie-broadcasts.store');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    
+    Route::apiResource('movies', MovieController::class);
+    
+    Route::get('movie-broadcasts', [MovieBoradcastController::class, 'index'])->name('movie-broadcasts.index');
+    Route::get('movie-broadcasts/{movie_broadcast}', [MovieBoradcastController::class, 'show'])->name('movie-broadcasts.show');
+    Route::post('movie-broadcasts', [MovieBoradcastController::class, 'store'])->name('movie-broadcasts.store');
+    
+    Route::get('movies/{movie}/movie-broadcasts', [MovieMovieBoradcastsController::class, 'index'])->name('movies.movie-broadcasts.index');
+    Route::get('movies/{movie}/movie-broadcasts/{movie_broadcast}', [MovieMovieBoradcastsController::class, 'show'])->name('movies.movie-broadcasts.show');
+    Route::post('movies/{movie}/movie-broadcasts', [MovieMovieBoradcastsController::class, 'store'])->name('movies.movie-broadcasts.store');
+});
