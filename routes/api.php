928f3b523e4cda13dc3b3f3b48aaef80425a2eae
Kristaps Līdacis <lidacisk@gmail.com>
2024-06-15 10:32:57 +0300

    Change MovieBroadcasts routes from apiResource to only used GET/POST routes

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,10 @@ use App\Http\Controllers\Api\MovieMovieBoradcastsController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('movies', MovieController::class);
-Route::apiResource('movie-broadcasts', MovieBoradcastController::class);
+
+Route::get('movie-broadcasts', [MovieBoradcastController::class, 'index'])->name('movie-broadcasts.index');
+Route::get('movie-broadcasts/{movie_broadcast}', [MovieBoradcastController::class, 'show'])->name('movie-broadcasts.show');
+Route::post('movie-broadcasts', [MovieBoradcastController::class, 'store'])->name('movie-broadcasts.store');
 
 Route::get('movies/{movie}/movie-broadcasts', [MovieMovieBoradcastsController::class, 'index'])->name('movies.movie-broadcasts.index');
 Route::get('movies/{movie}/movie-broadcasts/{movie_broadcast}', [MovieMovieBoradcastsController::class, 'show'])->name('movies.movie-broadcasts.show');
